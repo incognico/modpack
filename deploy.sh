475ddf7ec3247e027f8980a6f9297a15e57cdd80
incognico <nico@lifeisabug.com>
2020-04-26 16:35:07 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
 
+mod=~/.xonotic/smb/modpack
+xon=~/.xonotic/data
+www=/srv/www/distfiles.lifeisabug.com/htdocs/xonotic
 tag=`git describe --tags upstream/master`
 
-cp ~/.xonotic/smb/modpack/*.dat ~/.xonotic/data/.
-cp ~/.xonotic/smb/modpack/.cache/csprogs-$tag.pk3 ~/.xonotic/data/.
-cp ~/.xonotic/smb/modpack/.cache/csprogs-$tag.pk3 /srv/www/distfiles.lifeisabug.com/htdocs/xonotic/.
+cp $mod/*.dat $xon/.
+cp $mod/.cache/csprogs-$tag.pk3 $xon/.
+
+for i in $xon/*.pk3
+do
+   ln -sf $i $www/`basename $i`
+done
+
+find $www -xtype l -delete
